e437e352b647cf8f54054437c9ff409a4e81db4e
Cris Daniluk <cdaniluk@rhythmictech.com>
2022-03-14 09:15:38 -0400

    remove comment

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -18,32 +18,3 @@ resource "aws_s3_bucket_notification" "cloudtrail_notification" {
     lambda_function_arn = try(aws_cloudformation_stack.datadog_forwarder[0].outputs.DatadogForwarderArn, "")
   }
 }
-
-# data "aws_iam_policy_document" "cloudtrail" {
-#   statement {
-#     actions = ["sts:AssumeRole"]
-
-#     principals {
-#       type        = "AWS"
-#       identifiers = ["arn:aws:iam::${var.datadog_account_id}:root"]
-#     }
-
-#     condition {
-#       test     = "StringEquals"
-#       variable = "sts:ExternalId"
-#       values   = [datadog_integration_aws.datadog.external_id]
-#     }
-#   }
-# }
-
-# resource "aws_iam_policy" "cloudtrail" {
-#   name_prefix = "${var.name}-CloudTrail"
-#   path        = "/"
-#   policy      = data.aws_iam_policy_document.json
-#   tags        = local.tags
-# }
-
-# resource "aws_iam_role_policy_attachment" "cloudtrail" {
-#   role       = aws_iam_role.datadog.name
-#   policy_arn = aws_iam_policy.cloudtrail.arn
-# }
