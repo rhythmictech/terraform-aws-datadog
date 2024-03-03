11bc3de04495cb360f31f528def9e2ef344072b5
Cris Daniluk <cdaniluk@rhythmictech.com>
2024-03-03 18:10:20 -0500

    housekeeping

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,13 +1,23 @@
 terraform {
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.2.0"
+    }
+
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.74"
+      version = ">= 4.62"
     }
 
     datadog = {
       source  = "datadog/datadog"
-      version = "~>3.8"
+      version = ">= 3.37"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.0"
     }
   }
 }
