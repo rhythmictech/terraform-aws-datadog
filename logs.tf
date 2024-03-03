9a9908582edf5a547848f98c029c750135b41a10
Cris Daniluk <cdaniluk@rhythmictech.com>
2024-03-02 22:31:43 -0500

    refine

--- logs.tf
+++ logs.tf
@@ -2,37 +2,41 @@ data "datadog_logs_indexes" "indexes" {}
 
 
 resource "datadog_logs_index" "main" {
-  name           = "main"
-  retention_days = 15
+  count = var.logs_manage_indexes ? 1 : 0
+
+  name                                     = "main"
+  retention_days                           = 15
+  daily_limit                              = var.logs_daily_limit_main
+  daily_limit_warning_threshold_percentage = var.logs_daily_volume_warn_threshold_main
 
   filter {
-    query = "*"
+    query = ""
   }
 }
 
 resource "datadog_monitor" "daily_log_volume" {
-  count = var.daily_log_volume_enabled ? length(data.datadog_logs_indexes.indexes.logs_indexes) : 0
+  count = var.logs_manage_indexes && var.logs_daily_volume_enabled ? length(data.datadog_logs_indexes.indexes.logs_indexes) : 0
 
   name    = "Rolling 24h Log Usage Approaching Quota - ${data.datadog_logs_indexes.indexes.logs_indexes[count.index].name}"
-  message = var.daily_log_volume_message
+  message = var.logs_daily_volume_message
   type    = "log alert"
 
-  query = "logs(\"*\").index(\"main\").rollup(\"count\").last(\"1d\") > ${data.datadog_logs_indexes.indexes.logs_indexes[count.index].daily_limit * var.daily_log_volume_alert_threshold}"
+  query = "logs(\"*\").index(\"main\").rollup(\"count\").last(\"1d\") > ${data.datadog_logs_indexes.indexes.logs_indexes[count.index].daily_limit * var.logs_daily_volume_alert_threshold}"
 
   monitor_thresholds {
-    warning  = data.datadog_logs_indexes.indexes.logs_indexes[count.index].daily_limit * var.daily_log_volume_warn_threshold
-    critical = data.datadog_logs_indexes.indexes.logs_indexes[count.index].daily_limit * var.daily_log_volume_alert_threshold
+    warning  = data.datadog_logs_indexes.indexes.logs_indexes[count.index].daily_limit * var.logs_daily_volume_warn_threshold
+    critical = data.datadog_logs_indexes.indexes.logs_indexes[count.index].daily_limit * var.logs_daily_volume_alert_threshold
   }
 }
 
 resource "datadog_monitor" "anomalous_log_volume" {
-  count = var.anomalous_log_volume_enabled ? 1 : 0
+  count = var.logs_anomalous_volume_enabled ? 1 : 0
 
-  name    = "Log Usage vs 7 Day Avg - ${data.datadog_logs_indexes.indexes.logs_indexes[count.index].name}"
-  message = var.anomalous_log_volume_message
+  name    = "Anomalous Log Usage - ${data.datadog_logs_indexes.indexes.logs_indexes[count.index].name}"
+  message = var.logs_anomalous_volume_message
   type    = "query alert"
 
-  query = "avg(last_2d):anomalies(sum:datadog.estimated_usage.logs.ingested_events{datadog_is_excluded:false} by {var.anomalous_log_volume_grouped_sources}.as_count(), 'agile', 2, direction='both', interval=600, alert_window='${var.anomalous_log_volume_alert_window}', count_default_zero='true', seasonality='daily') >= 1"
+  query = "avg(${var.logs_anomalous_volume_alert_window}):anomalies(sum:datadog.estimated_usage.logs.ingested_events{datadog_is_excluded:false} by {var.anomalous_log_volume_grouped_sources}.as_count(), 'agile', 2, direction='both', interval=600, alert_window='${var.logs_anomalous_volume_alert_window}', count_default_zero='true', seasonality='daily') >= 1"
 
   monitor_thresholds {
     critical = 1
--- variables.tf
+++ variables.tf
@@ -236,25 +236,25 @@ variable "install_rds_enhanced_monitoring_lambda" {
 ########################################
 # Anomalous Log Volume (against quota)
 ########################################
-variable "anomalous_log_volume_enabled" {
+variable "logs_anomalous_volume_enabled" {
   default     = false
   description = "Whether or not to enable anomalous log usage monitoring"
   type        = bool
 }
 
-variable "anomalous_log_volume_alert_window" {
+variable "logs_anomalous_volume_alert_window" {
   default     = "last_2h"
   description = "Anomalous alert alert window (shorter thresholds may fire more often; see Datadog API for values)"
   type        = string
 }
 
-variable "anomalous_log_volume_grouped_sources" {
+variable "logs_anomalous_volume_grouped_sources" {
   default     = "datadog_index,service"
   description = "Comma separated sources to group by (include at least `datadog_index` for best results)"
   type        = string
 }
 
-variable "anomalous_log_volume_message" {
+variable "logs_anomalous_volume_message" {
   default     = "Anomalous spike on indexed logs for service {{service.name}}"
   description = "Default message for alert"
   type        = string
@@ -263,26 +263,47 @@ variable "anomalous_log_volume_message" {
 ########################################
 # Daily Log Volume (against quota)
 ########################################
-variable "daily_log_volume_enabled" {
+variable "logs_volume_enabled" {
   default     = false
   description = "Whether or not to enable daily log usage monitoring"
   type        = bool
 }
 
-variable "daily_log_volume_message" {
+variable "logs_daily_volume_message" {
   default     = "Unexpected spike on indexed logs for service {{service.name}}"
   description = "Default message for alert"
   type        = string
 }
 
-variable "daily_log_volume_alert_threshold" {
+variable "logs_daily_volume_alert_threshold" {
   default     = 0.9
   description = "Percentage threshold to alert on index log volume"
   type        = number
 }
 
-variable "daily_log_volume_warn_threshold" {
+variable "logs_daily_volume_warn_threshold" {
   default     = 0.7
   description = "Percentage threshold to warn on index log volume"
   type        = number
+}
+
+########################################
+# Main Index Config
+########################################
+variable "logs_daily_limit_main" {
+  description = "Daily log limit for the main index"
+  type        = number
+  // You can set a default value or leave it without to enforce user input
+}
+
+variable "logs_daily_volume_warn_threshold_main" {
+  default     = 0.9
+  description = "Warning threshold for daily log volume for the main index"
+  type        = number
+}
+
+variable "logs_manage_indexes" {
+  description = "A boolean flag to manage Datadog log indexes"
+  type        = bool
+  default     = false
 }
