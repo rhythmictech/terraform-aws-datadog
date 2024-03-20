94970319e7dee361f6f234cee291233fe5c014dc
Cris Daniluk <cdaniluk@rhythmictech.com>
2024-03-20 08:10:41 -0400

    add output to get datadog user when in govcloud

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "iam_role_datadog" {
   value       = try(aws_iam_role.datadog[0].name, "")
 }
 
+output "iam_user_datadog" {
+  description = "IAM user accessed by Datadog resources (when `access_method == user`)"
+  value       = try(aws_iam_user.datadog[0].name, "")
+}
+
 output "lambda_arn_forwarder" {
   description = "DataDog Lambda Forwarder ARN"
   value       = try(aws_cloudformation_stack.datadog_forwarder[0].outputs.DatadogForwarderArn, "")
